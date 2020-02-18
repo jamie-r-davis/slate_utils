b2349ef286b7a93a2f75ae23ae9b59eafecfcf0f
jamie-r-davis <jamjam@umich.edu>
2020-02-18 13:30:32 -0500

    Refactor to use session object instead of username/password

--- slate_utils/imports.py
+++ slate_utils/imports.py
@@ -4,10 +4,8 @@ import requests
 
 class Importer:
 
-    def __init__(self, hostname, username, password):
-        self.hostname = hostname.replace('https://', '')
-        self.username = username
-        self.password = password
+    def __init__(self, session):
+        self.session = session
 
     def force_pickup(self, verbose=False):
         """
@@ -19,7 +17,7 @@ class Importer:
             When True, the
         """
         url = f"https://{self.hostname}/manage/service/import?cmd=pickup"
-        r = requests.get(url, auth=(self.username, self.password), stream=True, hooks={'response': print_response})
+        r = self.session.get(url, stream=True, hooks={'response': print_response})
         r.raise_for_status()
 
     def force_import(self, verbose=False):
@@ -27,7 +25,7 @@ class Importer:
         Trigger a force import.
         """
         url = f"http://{self.hostname}/manage/import/load?cmd=process"
-        r = requests.get(url, auth=(self.username, self.password), stream=True, hooks={'response': print_response})
+        r = self.session.get(url, stream=True, hooks={'response': print_response})
         r.raise_for_status()
 
 def print_response(r, *args, **kwargs):
