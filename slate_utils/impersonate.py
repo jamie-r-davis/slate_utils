500e5d22ab89df07cf532705ac7bd419b73e8374
jamie-r-davis <jamjam@umich.edu>
2020-02-18 13:30:11 -0500

    Tidyup via Black

--- slate_utils/impersonate.py
+++ slate_utils/impersonate.py
@@ -1,5 +1,7 @@
 from contextlib import contextmanager
 from urllib.parse import urlencode
+
+
 from loguru import logger
 
 from selenium.webdriver.common.by import By
@@ -25,54 +27,53 @@ class Impersonator:
 
     def _impersonate(self, user):
         """Impersonate the giver user account where `user` is a guid.
-        
+
         Parameters
         ----------
         user : str
             The guid of the user account to impersonate.
-        
+
         """
         d = self.d
-        qs = {'id': user,
-              'view': 'active'}
-        url = self.base_url + '/manage/database/security/user?' + urlencode(qs)
+        qs = {"id": user, "view": "active"}
+        url = self.base_url + "/manage/database/security/user?" + urlencode(qs)
         d.get(url)
-        if d.title == '503 Error':
+        if d.title == "503 Error":
             raise UserDoesNotExistException(f"{user}: User does not exist.")
         wait = WebDriverWait(d, 10)
-        wait.until(EC.visibility_of_element_located((By.TAG_NAME, 'h1')))
-        d.find_element_by_link_text('Impersonate').click()
+        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "h1")))
+        d.find_element_by_link_text("Impersonate").click()
         alert = WebDriverWait(d, 10).until(EC.alert_is_present())
         alert.accept()
         return self.d
 
     def _unimpersonate(self):
         """Unimpersonate the current user.
-        
+
         """
         try:
-            d.find_element_by_link_text('Exit Impersonation')
+            d.find_element_by_link_text("Exit Impersonation")
         except NoSuchElementException:
             raise NotImpersonatedSessionException
-        redirect = self.base_url + '/manage'
-        qs = {'r': redirect}
-        url = self.base_url + '/manage/logout?' + urlencode(qs)
+        redirect = self.base_url + "/manage"
+        qs = {"r": redirect}
+        url = self.base_url + "/manage/logout?" + urlencode(qs)
         self.d.get(url)
         return self.d
 
     @contextmanager
     def impersonate(self, user):
         """Context manager that will impersonate the given `user`, returning
-        a driver instance for the impersonated session. 
-        
-        After the context manager is closed, the impersonated session 
+        a driver instance for the impersonated session.
+
+        After the context manager is closed, the impersonated session
         will be terminated.
-        
+
         Parameters
         ----------
         user : str
             The guid of the user account to impersonate.
-        
+
         """
         logger.debug(f"Impersonating {user}...")
         self._impersonate(user)
